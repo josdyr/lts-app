5cda5a159dc1847803c6d28f2b50311b101b9307
Jostein Dyrseth <josdyr@gmail.com>
2023-12-21 02:37:25 +0100

    Update map height and center coordinates

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 export const Home = () => {
   const containerStyle = {
     width: "100%",
-    height: "750px",
+    height: "730px",
   };
 
   // Coordinates for the cities
@@ -13,19 +13,21 @@ export const Home = () => {
 
   // Center the map around southern Norway
   const center = {
-    lat: (stavanger.lat + bergen.lat + oslo.lat) / 3,
-    lng: (stavanger.lng + bergen.lng + oslo.lng) / 3,
+    lat: 59.913869,
+    lng: 7.752245,
   };
 
   const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
   return (
     <div className="appContainer">
+      <div>Home</div>
       <LoadScript googleMapsApiKey={apiKey}>
         <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={7}>
           <Marker position={stavanger} />
           <Marker position={bergen} />
           <Marker position={oslo} />
+          {/* You can add InfoWindows or custom components here to show details */}
         </GoogleMap>
       </LoadScript>
     </div>
