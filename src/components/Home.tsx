36e33d91f18b4ce5e700c6f38e1be9304d685928
Jostein Dyrseth <josdyr@gmail.com>
2023-12-21 01:13:03 +0100

    Add Google Maps API key to environment variables

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,7 @@
-// import { Loader } from "@googlemaps/js-api-loader";
-
 export const Home = () => {
-  // const loader = new Loader({
-  //   apiKey: "AIzaSyAeZpaGJxDuBV0qm_jWGSdgGQfju0LiklM",
-  //   version: "weekly",
-  //   // ...additionalOptions,
-  // });
-
-  // loader.importLibrary().then(async () => {
-  //   const { Map } = await google.maps.importLibrary("maps");
-
-  //   map = new Map(document.getElementById("map"), {
-  //     center: { lat: -34.397, lng: 150.644 },
-  //     zoom: 8,
-  //   });
-  // });
-
   return (
     <div className="appContainer">
       <div>Home</div>
-      {/* <div id="map"></div> */}
     </div>
   );
 };
