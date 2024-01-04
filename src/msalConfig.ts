83164dabff0a2b70f028bc059f738db1cddf3af2
Jostein Dyrseth <josdyr@gmail.com>
2024-01-04 18:33:59 +0100

    Update .env and msalConfig.ts

--- src/msalConfig.ts
+++ src/msalConfig.ts
@@ -5,6 +5,8 @@ export const msalConfig: Configuration = {
     clientId: "f0920ef7-01a9-48b1-bbd0-72dae6ef3869",
     authority:
       "https://login.microsoftonline.com/1a3889b2-f76f-4dd8-831e-b2d5e716c986",
-    redirectUri: import.meta.env.VITE_APP_URL,
+    redirectUri: import.meta.env.DEV
+      ? "http://localhost:5173"
+      : import.meta.env.VITE_APP_URL,
   },
 };
